0c86999f2133e58db5c53bf89fdce6527105854c
Auto Sync <auto-sync@localhost>
2026-03-10 01:19:30 +0600

    remove tmp from repo

--- tmp/verify_tables.php
+++
@@ -1,6 +0,0 @@
-<?php
-// /tmp/verify_tables.php
-$mysqli = new mysqli("localhost", "tdhuedhn_broxbhai", "EnTio1PtqI-&M&D", "tdhuedhn_broxbhai");
-$res = $mysqli->query("SHOW TABLES LIKE 'ai_%'");
-while($row = $res->fetch_array()) echo $row[0]."\n";
-$mysqli->close();
