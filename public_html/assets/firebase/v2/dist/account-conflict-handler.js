e45fecaf7c22918dce85db56471bdf8d4875182e
Auto Sync <auto-sync@localhost>
2026-03-14 00:56:31 +0600

    fix: update .gitignore and remove tracked files

--- public_html/assets/firebase/v2/dist/account-conflict-handler.js
+++
@@ -1,9 +0,0 @@
-import {
-  AccountConflictHandler,
-  account_conflict_handler_default
-} from "./chunks/chunk-RUSWDHIF.js";
-import "./chunks/chunk-OIR3NABF.js";
-export {
-  AccountConflictHandler,
-  account_conflict_handler_default as default
-};
