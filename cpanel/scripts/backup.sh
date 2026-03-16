2b063c41081007416dc8f8c38b694e8c54581059
habibbrox2 <hrhabib.hrs@gmail.com>
2026-03-16 13:31:53 +0600

    feat: add automated setup script for cPanel deployment

--- cpanel/scripts/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-APP="/home/tdhuedhn/broxlab/app"
-BACKUPS="/home/tdhuedhn/broxlab/backups"
-
-DATE=$(date +"%Y%m%d_%H%M%S")
-
-tar -czf $BACKUPS/backup_$DATE.tar.gz $APP/current
--- cpanel/scripts/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-RELEASES="/home/tdhuedhn/broxlab/app/releases"
-
-ls -dt $RELEASES/* | tail -n +6 | xargs rm -rf
