cc9949c8b2f1e546e6d8f7eec543bafeb190982f
habibbrox2 <hrhabib.hrs@gmail.com>
2026-05-06 17:51:49 +0600

    cleanup: Remove setup and test script files
    - Remove check_roles.php
    - Remove check_settings.php
    - Remove setup_ai_providers.php
    - Remove setup_ai_settings.php
    - Remove setup_roles.php
    - Remove system/prompts/admin_utf8.md
    - Remove test_admin_chat.php
    - Remove test_admin_chat_http.php
    - Remove test_ai_provider.php
    - Remove update_api_keys.php

--- check_settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'Config/Db.php';
-global $mysqli;
-$result = $mysqli->query('SELECT setting_key, setting_value FROM ai_settings');
-while ($row = $result->fetch_assoc()) {
-    echo $row['setting_key'] . ': ' . $row['setting_value'] . "\n";
-}
