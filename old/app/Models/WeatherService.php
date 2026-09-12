ae4e9b77caba0ac26d983882d801fd0db08393b4
Habibur Rahman <hrhabib.hrs@gmail.com>
2026-09-13 02:00:06 +0600

    Delete old directory

--- old/app/Models/WeatherService.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Compatibility entrypoint for legacy code that expects `app/Models/WeatherService.php`
-// This file delegates to the compat shim which forwards calls to the new namespaced service.
-
-require_once __DIR__ . '/WeatherServiceCompat.php';
